8bf16e3a73e6818d37005ddcb36719e346039ad4
Deen <1732342@students.wits.ac.za>
2020-03-11 11:34:15 +0200

    Made Some Extra blog pages
    Updated BlogPages and Weekly reading

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log()
+// var a = 1; //old, dont use it.
+// let b = 2; // modern var. because dont break the web. 
+// const c = 3; // only assign to this variable once. it is safe.
+// let c = true;
+
+// 8 Types: Numbers. 1 complex, 7 primitaves. Boolean, null(memory reference set to nothing), undefined(not even a memmory reference), number, Bigint(large numbers) string and symbol. The Object is the complex.
+
+//false, null, 0, "", undefined, [] all, NaN = false faulty values
+// =, ==, ===.
+//you can also create classes in java. Its amazing because its a reliable container fpr "this" that will be set to an instance of a class. 
+
+
+
+//  So Javascripts main functionaly is that its functional. It is executable or allows for certain actions/ procedures to be excuted
+
+// Variables:
+/* <script>
+    {
+        <button>Press me</button>
+const button = document.querySelector('button');
+
+button.onclick = function() {
+        let name = prompt('What is your name?');
+    alert('Hello ' + name + ', nice to see you!');
+  }
+  }
+</script> *
+
+
+// Theres a button that says Press me. When you click it a prompt shows up that says What is your name with a space for you to input your name.  When you submit, an alert pops up that says nice to see you. We also now use let instead of var because its a more correct way of using js.
