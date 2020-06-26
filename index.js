c79c7095fb13d3d34a068fdcb7d0a484839e71c5
Deen <1732342@students.wits.ac.za>
2020-06-27 00:24:55 +0200

    java nav
    Javascript navigation changes

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-// console.log()
-// var a = 1; //old, dont use it.
-// let b = 2; // modern var. because dont break the web. 
-// const c = 3; // only assign to this variable once. it is safe.
-// let c = true;
 
-// 8 Types: Numbers. 1 complex, 7 primitaves. Boolean, null(memory reference set to nothing), undefined(not even a memmory reference), number, Bigint(large numbers) string and symbol. The Object is the complex.
+const initializeMenu = () => {
+    const nav = document.querySelector('nav');
+    const ul = document.createElement('ul');
 
-//false, null, 0, "", undefined, [] all, NaN = false faulty values
-// =, ==, ===.
-//you can also create classes in java. Its amazing because its a reliable container fpr "this" that will be set to an instance of a class. 
+    for (let item of ['Home', 'Contact me', 'Blogs', 'About Me', 'Site details']) {
+        const li = document.createElement('li');
+        li.innerText = item;
+
+        ul.appendChild(li);
+    }
+
+    nav.appendChild(ul);
+};
+
+document.addEventListener{ 'DOMContentLoaded', () => initializeMenu() };
+
+    // <title id="blogs">Blog Navigation</title>
+    // var blogNav = document.getElementById("blogs").innerHTML;
+    // var blogNav = document.getElementById("blogs").innerHTML.nodeValue;
+
+    // let blogs = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8']
+    // console.log(blogs.length)
+
+    // let first = blogs[0]
+    // let last = blogs[blogs.lenght - 1]
+
+    // "index.html">Home Page</a></li>
+    // <li><a href="blogs.html">Blogs</a></li>
+    // <li><a href="characterBlog.html">About Me</a></li>
+    // <li><a href="siteDetails.html">Site details</a></li>
+    // <li><a href="notes.html">Notes</a></li>
+    // <li><a href="contactMe.html">Contact Me</a></li>
 
 
-
-//  So Javascripts main functionaly is that its functional. It is executable or allows for certain actions/ procedures to be excuted
-
-//<script>
-{
-    <button>Help</button>
-    const button = document.querySelector('button');
-
-    button.onclick = function () {
-        let name = prompt('Can I help you?');
-        alert('Hello ' + name + ', nice to see you!');
-
-
-
-        // the random number generator game
-        //{
-        //  let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-        //  // const guesses = document.querySelector('.guesses');
-        //   const lastResult = document.querySelector('.lastResult');
-        //   const lowOrHi = document.querySelector('.lowOrHi');
-
-        //   const guessSubmit = document.querySelector('.guessSubmit');
-        //   const guessField = document.querySelector('.guessField');
-
-        //   let guessCount = 1;
-        //   let resetButton;
-        // }
-
-        // </script>
-
-        // Theres a button that says Press me. When you click it a prompt shows up that says What is your name with a space for you to input your name.  When you submit, an alert pops up that says nice to see you. We also now use let instead of var because its a more correct way of using js.
-
-        <title id="blogs">Blog Navigation</title>
-        var blogNav = document.getElementById("blogs").innerHTML;
-        var blogNav = document.getElementById("blogs").innerHTML.nodeValue;
-
-        let blogs = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8']
-        console.log(blogs.length)
-
-        let first = blogs[0]
-        let last = blogs[blogs.lenght - 1]
 
 
 
